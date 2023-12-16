aba726147b2535039ccadd9691d9ca3a5b6cbdb6
adivaibhav <67663916+adivaibhav@users.noreply.github.com>
2023-12-16 06:55:57 +0530

    Update BMS-Master.ino

--- BMS-Master.ino
+++ BMS-Master.ino
@@ -1,8 +1,22 @@
+#include <Arduino.h>
+#include <stdint.h>
+#include <SPI.h>
+#include "Linduino.h"
+#include "LT_SPI.h"
+#include "UserInterface.h"
+#include "LTC681x.h"
+#include "LTC6813.h"
 #include <FlexCAN_T4.h>
 #include "arduino_freertos.h"
 #include "avr/pgmspace.h"
 #include <semphr.h>
 
+#define ENABLED 1
+#define DISABLED 0
+#define DATALOG_ENABLED 1
+#define DATALOG_DISABLED 0
+#define PWM 1
+#define SCTL 2
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
 CAN_message_t msg;
 CAN_message_t receivedMsg;
@@ -14,6 +28,74 @@ unsigned long value3;
 SemaphoreHandle_t adcMutex;
 bool adcValueReady = false;
 QueueHandle_t canQueue;
+
+
+const uint8_t TOTAL_IC = 6;//!< Number of ICs in the daisy chain
+
+/********************************************************************
+ ADC Command Configurations. See LTC681x.h for options
+*********************************************************************/
+const uint8_t ADC_OPT = ADC_OPT_DISABLED; //!< ADC Mode option bit
+const uint8_t ADC_CONVERSION_MODE =MD_7KHZ_3KHZ; //!< ADC Mode
+const uint8_t ADC_DCP = DCP_DISABLED; //!< Discharge Permitted 
+const uint8_t CELL_CH_TO_CONVERT =CELL_CH_ALL; //!< Channel Selection for ADC conversion
+const uint8_t AUX_CH_TO_CONVERT = AUX_CH_ALL; //!< Channel Selection for ADC conversion
+const uint8_t STAT_CH_TO_CONVERT = STAT_CH_ALL; //!< Channel Selection for ADC conversion
+const uint8_t SEL_ALL_REG = REG_ALL; //!< Register Selection 
+const uint8_t SEL_REG_A = REG_1; //!< Register Selection 
+const uint8_t SEL_REG_B = REG_2; //!< Register Selection 
+
+const uint16_t MEASUREMENT_LOOP_TIME = 500; //!< Loop Time in milliseconds(ms)
+
+//Under Voltage and Over Voltage Thresholds
+const uint16_t OV_THRESHOLD = 41000; //!< Over voltage threshold ADC Code. LSB = 0.0001 ---(4.1V)
+const uint16_t UV_THRESHOLD = 30000; //!< Under voltage threshold ADC Code. LSB = 0.0001 ---(3V)
+
+//Loop Measurement Setup. These Variables are ENABLED or DISABLED. Remember ALL CAPS
+const uint8_t WRITE_CONFIG = DISABLED;  //!< This is to ENABLED or DISABLED writing into to configuration registers in a continuous loop
+const uint8_t READ_CONFIG = DISABLED; //!< This is to ENABLED or DISABLED reading the configuration registers in a continuous loop
+const uint8_t MEASURE_CELL = ENABLED; //!< This is to ENABLED or DISABLED measuring the cell voltages in a continuous loop
+const uint8_t MEASURE_AUX = DISABLED; //!< This is to ENABLED or DISABLED reading the auxiliary registers in a continuous loop
+const uint8_t MEASURE_STAT = DISABLED; //!< This is to ENABLED or DISABLED reading the status registers in a continuous loop
+const uint8_t PRINT_PEC = DISABLED; //!< This is to ENABLED or DISABLED printing the PEC Error Count in a continuous loop
+
+cell_asic BMS_IC[TOTAL_IC]; //!< Global Battery Variable
+
+/*************************************************************************
+ Set configuration register. Refer to the data sheet
+**************************************************************************/
+bool REFON = true; //!< Reference Powered Up Bit
+bool ADCOPT = false; //!< ADC Mode option bit
+bool GPIOBITS_A[5] = {false,false,true,true,true}; //!< GPIO Pin Control // Gpio 1,2,3,4,5
+bool GPIOBITS_B[4] = {false,false,false,false}; //!< GPIO Pin Control // Gpio 6,7,8,9
+uint16_t UV=UV_THRESHOLD; //!< Under voltage Comparison Voltage
+uint16_t OV=OV_THRESHOLD; //!< Over voltage Comparison Voltage
+bool DCCBITS_A[12] = {false,false,false,false,false,false,false,false,false,false,false,false}; //!< Discharge cell switch //Dcc 1,2,3,4,5,6,7,8,9,10,11,12
+bool DCCBITS_B[7]= {false,false,false,false,false,false,false}; //!< Discharge cell switch //Dcc 0,13,14,15
+bool DCTOBITS[4] = {true,false,true,false}; //!< Discharge time value //Dcto 0,1,2,3  // Programed for 4 min 
+/*Ensure that Dcto bits are set according to the required discharge time. Refer to the data sheet */
+bool FDRF = false; //!< Force Digital Redundancy Failure Bit
+bool DTMEN = true; //!< Enable Discharge Timer Monitor
+bool PSBITS[2]= {false,false}; //!< Digital Redundancy Path Selection//ps-0,1
+
+
+void setup()
+{
+  Serial.begin(115200);
+  quikeval_SPI_connect();
+  spi_enable(SPI_CLOCK_DIV16); // This will set the Linduino to have a 1MHz Clock
+  LTC6813_init_cfg(TOTAL_IC, BMS_IC);
+  LTC6813_init_cfgb(TOTAL_IC,BMS_IC);
+  for (uint8_t current_ic = 0; current_ic<TOTAL_IC;current_ic++) 
+  {
+    LTC6813_set_cfgr(current_ic,BMS_IC,REFON,ADCOPT,GPIOBITS_A,DCCBITS_A, DCTOBITS, UV, OV);
+    LTC6813_set_cfgrb(current_ic,BMS_IC,FDRF,DTMEN,PSBITS,GPIOBITS_B,DCCBITS_B);   
+  }   
+  LTC6813_reset_crc_count(TOTAL_IC,BMS_IC);
+  LTC6813_init_reg_limits(TOTAL_IC,BMS_IC);
+    wakeup_sleep(TOTAL_IC);
+}
+
 
 static void task1(void*) {
     while (true) {
