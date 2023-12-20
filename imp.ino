27505d6a564d01d1d51d83acbad94bb62fbe35af
adivaibhav <67663916+adivaibhav@users.noreply.github.com>
2023-12-20 10:17:32 +0530

    Update imp.ino

--- imp.ino
+++ imp.ino
@@ -26,3 +26,35 @@
       check_error(error);
       print_sumofcells();
       break;
+
+
+void print_cells(uint8_t datalog_en)
+{
+  for (int current_ic = 0 ; current_ic < TOTAL_IC; current_ic++)
+  {
+    if (datalog_en == 0)
+    {
+      Serial.print(" IC ");
+      Serial.print(current_ic+1,DEC);
+      Serial.print(", ");
+      for (int i=0; i<BMS_IC[0].ic_reg.cell_channels; i++)
+      {
+        Serial.print(" C");
+        Serial.print(i+1,DEC);
+        Serial.print(":");
+        Serial.print(BMS_IC[current_ic].cells.c_codes[i]*0.0001,4);
+        Serial.print(",");
+      }
+      Serial.println();
+    }
+    else
+    {
+      Serial.print(" Cells, ");
+      for (int i=0; i<BMS_IC[0].ic_reg.cell_channels; i++)
+      {
+        Serial.print(BMS_IC[current_ic].cells.c_codes[i]*0.0001,4);
+        Serial.print(",");
+      }
+    }
+  }
+  Serial.println("\n");
