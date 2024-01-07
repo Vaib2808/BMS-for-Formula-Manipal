6ea50a7292327ca10130d9dce02545f4136886cb
Vaibhav <vaibhav.mitmpl2022@learner.manipal.edu>
2024-01-07 10:45:50 +0530

    Update BMS-Master.ino

--- BMS-Master.ino
+++ BMS-Master.ino
@@ -4,3 +4,31 @@ FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
 CAN_message_t msg;
 
 
+
+
+
+
+
+
+
+
+ void canint(const CAN_message_t &receivedMsg) {
+//  can1.events();
+            value3 = receivedMsg.id;
+          if(receivedMsg.id == 0x1838F380){
+            ::Serial.print("CAN ID:  ");
+            ::Serial.print(receivedMsg.id,arduino:: HEX );
+            ::Serial.print(" ");
+//            dataTherm = recievedMs
+            for(int k=0;k<8;k++){
+             dataTherm[k] =  (int8_t)receivedMsg.buf[k];
+              ::Serial.print(dataTherm[k]);
+               ::Serial.print(" ");
+              }
+              ::Serial.println();
+//            ::vTaskDelay(pdMS_TO_TICKS(30));
+//        }
+          }
+}
+
+
